0ad4c63292e72f1f56e27eaa5b352dc33ac333ab
danafhoppe <danafhoppe@gmail.com>
2020-06-26 04:44:53 -0500

    Updated HTML Code

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'danafhoppe@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "Username: $firstname.\n".
+        "User LastName: $lastname.\n".
+            "User Email: $email.\n".
+                "User Message: $message.\n";
+    $to = "danafhoppe@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+?>
